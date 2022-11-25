60c4806180ff7b8d02e61b69c6a63c2e3d13e8d3
shansila <shansilapp@gmail.com>
2022-11-25 17:30:06 +0400

    messages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) }];
+const routes: Routes = [
+  {path:'home',component:CustomerComponent},
+
+  
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
